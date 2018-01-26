f0f132cdf42355c29c4cd2a0ddb2b0292147ae27
Angel Del Castillo <jubu201504@gmail.com>
2018-01-26 12:46:02 -0600

    Added json2struct util.

--- utils.sh
+++ utils.sh
@@ -2,8 +2,18 @@
 
 echo "Loaded *NIX shell utils."
 
+## GIT
+
 alias gts='git status';
 alias gpull='git pull origin $1';
 alias gmdev='git pull origin develop; git pull origin $1';
+
+## MISC
+
 alias pwdcopy='pwd|pbcopy';
 
+## JSON to Struct
+# Example: JSON=$(cat ~/Desktop/swagger20.json); json2struct "$JSON" | pbcopy
+
+UDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+alias json2struct='node $UDIR/app.js $1';
